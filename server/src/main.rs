38175e48ff8b345ee9aea9f1cad20ce75faf560b
Phillip Seeliger <phillip.seeliger@gmail.com>
2025-08-20 23:16:50 +0200

    feat(server): Add command line arg for listen address

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 mod chat_server;
 
 use crate::chat_server::chat_server::ChatServer;
+use std::{env};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool};
 
@@ -8,11 +9,13 @@ use std::sync::atomic::{AtomicBool};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
+    let args: Vec<String> = env::args().collect();
+    let server_addr = args[1].clone();
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
 
     let mut chat_server = ChatServer::new();
-    chat_server.listen("0.0.0.0:8081");
+    chat_server.listen(format!("{}", server_addr));
 
     tokio::signal::ctrl_c().await.expect("Failed to listen for SIGINT");
     println!("Received SIGINT: Shutting down gracefully...");
