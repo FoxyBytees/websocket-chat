94f78a920deb38af1ec1a9ee8da7a1f3c7577ffc
Phillip Seeliger <phillip.seeliger@gmail.com>
2025-08-22 00:33:16 +0200

    wip(client): prototype menu for chosing login, register and user to send message to

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,8 +5,7 @@ use chrono::{DateTime, Local};
 use colored::*;
 use std::{env};
 use std::io::{stdout, stdin, Write};
-use std::sync::Arc;
-use std::sync::RwLock;
+use std::sync::{Arc,RwLock};
 use protocol::ChatMessage;
 
 #[tokio::main]
@@ -15,16 +14,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = env::args().collect();
     let server_addr = args[1].clone();
-    let from_user = args[2].clone();
-    let from_user_clone = from_user.clone();
-    let to_user = args[3].clone();
+    let username = Arc::new(RwLock::new(String::new()));
+    let username_clone = username.clone();
     let msg_list = Arc::new(RwLock::new(Vec::<ChatMessage>::new()));
 
     let mut chat_client = ChatClient::new();
-    chat_client.connect(format!("ws://{}/", server_addr), move |chat_msg| {
+    chat_client.connect(
+        format!("ws://{}/", server_addr),
+        move |chat_msg| {
         print!("\x1B[2J\x1B[1;1H");
+        let username = username_clone.read().unwrap().trim().to_owned();
 
-        if chat_msg.src_user.trim() != from_user_clone {
+        if chat_msg.src_user.trim() != username {
             print!("\x07");
         }
 
@@ -34,8 +35,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let datetime_local: DateTime<Local> = chat_msg.send_time.into();
             let formatted_local = datetime_local.format("%d.%m.%Y %H:%M:%S").to_string();
 
-            if chat_msg.src_user.trim() != from_user_clone {
-                print!(
+            if chat_msg.src_user.trim() != username {
+                println!(
                     "[{}] {}: {}",
                     formatted_local.magenta(),
                     chat_msg.src_user.cyan(),
@@ -43,7 +44,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 );
             }
             else {
-                print!(
+                println!(
                     "[{}] {}: {}",
                     formatted_local.magenta(),
                     chat_msg.src_user.green(),
@@ -57,20 +58,55 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     }).await?;
 
-    chat_client.register(&*from_user, "TestPW").await?;
-    chat_client.login(&*from_user, "TestPW").await?;
+    loop{
+        print!("\x1B[2J\x1B[1;1H");
+        println!("1: Login");
+        println!("2: Register");
+        print!("Choice: ");
+        let mut choice = String::new();
+        stdout().flush().unwrap();
+        stdin().read_line(&mut choice)?;
+
+        print!("\x1B[2J\x1B[1;1H");
+        print!("Username: ");
+        stdout().flush().unwrap();
+        stdin().read_line(&mut username.write().unwrap())?;
+
+        print!("\x1B[2J\x1B[1;1H");
+        print!("Password: ");
+        let mut password = String::new();
+        stdout().flush().unwrap();
+        stdin().read_line(&mut password)?;
+
+        match choice.trim() {
+            "1" => {
+                chat_client.login(username.read().unwrap().trim(), password.trim()).await?;
+                break;
+            }
+            "2" => {
+                chat_client.register(username.read().unwrap().trim(), password.trim()).await?;
+            }
+            _ => {}
+        }
+    }
+
+    print!("\x1B[2J\x1B[1;1H");
+    print!("User to message: ");
+    let mut to_user = String::new();
+    stdout().flush().unwrap();
+    stdin().read_line(&mut to_user)?;
 
     loop {
         let mut message= String::new();
 
-        stdin().read_line(&mut message).expect("Failed to read line");
+        stdin().read_line(&mut message)?;
 
         if message.trim() == "exit()" {
             chat_client.disconnect().await?.wait_done().await?;
             break;
         }
 
-        chat_client.message(&*to_user, &*message).await?;
+        chat_client.message(to_user.trim(), message.trim()).await?;
     }
 
     Ok(())
